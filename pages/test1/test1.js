d0bb793a65843dd3938a24c312cdb80617846dde
tingzh <zt.melody@outlook.com>
2018-07-29 20:44:35 +0800

    layout learn

--- pages/test1/test1.js
+++ pages/test1/test1.js
@@ -2,7 +2,6 @@
 var util = require('../../utils/util.js')
 
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -10,7 +9,25 @@ Page({
     data: 'welcomegoodhunter',
     condition: true,
     arrayData: [1, 2, 3, 4, 5],
-    time: util.formatTime(new Date())
+    time: util.formatTime(new Date()),
+    navLeftItems: [1, 2, 3, 4, 5],
+    navRightItems: [
+      ['1-1', '1-2', '1-3', '1-4', '1-5'],
+      ['2-1', '2-2', '2-3', '2-4', '2-5'],
+      ['3-1', '3-2', '3-3', '3-4', '3-5'],
+      ['4-1', '4-2', '4-3', '4-4', '4-5'],
+      ['5-1', '5-2', '5-3', '5-4', '5-5']
+    ],
+    curNav: 1,
+    curIndex: 0,
+    currentItems: []
+  },
+  navLeftItemTap: function(e) {
+    let id = parseInt(e.target.dataset.id);
+    this.setData({
+      curNav: id,
+      currentItems: this.data.navRightItems[id - 1]
+    })
   },
   onPullDownRefresh: function() {
     // 显示导航栏loading
@@ -26,7 +43,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    this.setData({
+      currentItems: this.data.navRightItems[0]
+    })
   },
 
   /**
--- pages/test4/test4.js
+++ pages/test4/test4.js
@@ -5,7 +5,56 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    goodslist: [
+      {
+        id: "001",
+        imgUrl: "http://img5.imgtn.bdimg.com/it/u=2906541843,1492984080&fm=23&gp=0.jpg",
+        name: "女装T恤中长款大码摆裙春夏新款",
+        price: "65.00"
+      },
+      {
+        id: "002",
+        imgUrl: "http://img4.imgtn.bdimg.com/it/u=1004404590,1607956492&fm=23&gp=0.jpg",
+        name: "火亮春秋季 男青年修身款圆领男士T恤",
+        price: "68.00"
+      },
+      {
+        id: "003",
+        imgUrl: "http://img1.imgtn.bdimg.com/it/u=2305064940,3470659889&fm=23&gp=0.jpg",
+        name: "新款立体挂脖t恤女短袖大码宽松条纹V领上衣显瘦休闲春夏",
+        price: "86.00"
+      },
+      {
+        id: "004",
+        imgUrl: "http://img4.imgtn.bdimg.com/it/u=3986819380,1610061022&fm=23&gp=0.jpg",
+        name: "男运动上衣春季上新品 上衣流行装青年",
+        price: "119.00"
+      },
+      {
+        id: "005",
+        imgUrl: "http://img1.imgtn.bdimg.com/it/u=3583238552,3525141111&fm=23&gp=0.jpg",
+        name: "时尚字母三角露胸t恤女装亮丝大码宽松不规则春夏潮",
+        price: "69.00"
+      },
+      {
+        id: "006",
+        imgUrl: "http://img2.imgtn.bdimg.com/it/u=1167272381,3361826143&fm=23&gp=0.jpg",
+        name: "新款立体挂脖t恤短袖大码宽松条纹V领上衣显瘦休闲春夏",
+        price: "86.00"
+      },
+      {
+        id: "007",
+        imgUrl: "http://img0.imgtn.bdimg.com/it/u=789486313,2033571593&fm=23&gp=0.jpg",
+        name: "时尚字母三角露胸t恤女装亮丝大码宽松不规则春夏潮",
+        price: "119.00"
+      },
+      {
+        id: "008",
+        imgUrl: "http://img2.imgtn.bdimg.com/it/u=3314044863,3966877419&fm=23&gp=0.jpg",
+        name: "男运动上衣春季上新品 上衣流行装青年",
+        price: "69.00"
+      },
+    ]
   },
 
   /**
